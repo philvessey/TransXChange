3d3e362f68b4427cee3ece59e929cb044bd9a84c
Phil Vessey <philvessey@icloud.com>
2022-01-16 18:21:01 +0000

    Update copyright.

--- TransXChange.Wales/Properties/AssemblyInfo.cs
+++ TransXChange.Wales/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyCopyright("Copyright © 2021 Phil Vessey and Contributors")]
+[assembly: AssemblyCopyright("Copyright © 2022 Phil Vessey and Contributors")]
 [assembly: AssemblyTitle("TransXChange.Wales")]
 [assembly: AssemblyVersion("1.2.0.0")]
