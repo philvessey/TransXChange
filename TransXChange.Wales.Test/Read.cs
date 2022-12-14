46b1f6269329f21b9888a6e64e808c3a1942fede
Phil Vessey <philvessey@icloud.com>
2022-12-14 15:43:45 +0000

    Add support for filename indexes.

--- TransXChange.Wales.Test/Read.cs
+++ TransXChange.Wales.Test/Read.cs
@@ -29,7 +29,7 @@ namespace TransXChange.Wales.Test
             TravelineHelpers travelineHelpers = new TravelineHelpers();
             
             Dictionary<string, NAPTANStop> stops = naptanHelpers.Read("Data/cardiff.csv");
-            Dictionary<string, TXCSchedule> originals = travelineHelpers.ReadEngland(stops, "Data/W.zip", Environment.GetEnvironmentVariable("KEY"), "bus", new[] { "5710WDB48395" }, 7);
+            Dictionary<string, TXCSchedule> originals = travelineHelpers.ReadEngland(stops, "Data/W.zip", Environment.GetEnvironmentVariable("KEY"), "bus", new[] { "all" }, new[] { "5710WDB48395" }, 7);
 
             Assert.IsNotNull(originals);
         }
