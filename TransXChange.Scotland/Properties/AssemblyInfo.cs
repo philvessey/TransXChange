fbded3402cf9e710be841647ebba67b9aefdc061
Phil Vessey <philvessey@icloud.com>
2024-03-04 20:48:14 +0000

    Update copyright.

--- TransXChange.Scotland/Properties/AssemblyInfo.cs
+++ TransXChange.Scotland/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyCopyright("Copyright © 2023 Phil Vessey and Contributors")]
+[assembly: AssemblyCopyright("Copyright © 2024 Phil Vessey and Contributors")]
 [assembly: AssemblyTitle("TransXChange.Scotland")]
 [assembly: AssemblyVersion("2.2.3.0")]
