f77a8cfff46a884cc84412fb1a8e8fcec2c3c338
Phil Vessey <philvessey@icloud.com>
2024-04-20 10:57:55 +0100

    Update tests sample data.

--- TransXChange.Scotland.Test/Read.cs
+++ TransXChange.Scotland.Test/Read.cs
@@ -29,7 +29,7 @@ namespace TransXChange.Scotland.Test
             TravelineHelpers travelineHelpers = new TravelineHelpers();
             
             Dictionary<string, NAPTANStop> stops = naptanHelpers.Read("Data/edinburgh.csv");
-            Dictionary<string, TXCSchedule> schedules = travelineHelpers.ReadScotland(stops, "Data/S.zip", Environment.GetEnvironmentVariable("KEY"), "bus", new[] { "all" }, new[] { "6200206531" }, "13/11/2023", 7);
+            Dictionary<string, TXCSchedule> schedules = travelineHelpers.ReadScotland(stops, "Data/S.zip", Environment.GetEnvironmentVariable("KEY"), "bus", new[] { "all" }, new[] { "6200206531" }, "22/04/2024", 7);
 
             Assert.IsTrue(schedules.Count > 0);
         }
